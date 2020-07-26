c28a3795d5785b79d2086c8d444487dc99260331
Denis Luna <denis.lbs18@gmail.com>
2020-07-26 15:54:20 -0300

    Remoção de geração de dados.

--- src/main/java/br/com/petz/petz_crud/ApirestApplication.java
+++ src/main/java/br/com/petz/petz_crud/ApirestApplication.java
@@ -1,15 +1,7 @@
 package br.com.petz.petz_crud;
 
-import java.util.stream.LongStream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import br.com.petz.petz_crud.model.Cliente;
-import br.com.petz.petz_crud.repository.ClienteRepository;
-import br.com.petz.petz_crud.util.GeradorDeDados;
 
 @SpringBootApplication
 public class ApirestApplication {
@@ -18,20 +10,14 @@ public class ApirestApplication {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(ClienteRepository clienteRepository) {
-		return args -> {
-			clienteRepository.deleteAll();
-			LongStream.range(1, 4).mapToObj(i -> {
-				Cliente c = new Cliente();
-				c.setNome("Cliente " + i);
-				c.setEmail("cliente" + i + "@email.com");
-				c.setGenero(GeradorDeDados.geraGenero());
-				c.adicionaEndereco(GeradorDeDados.geraEndereco());
-				c.setIdade(GeradorDeDados.geraIntegerAleatorio(18, 75));
-				c.setCpf(GeradorDeDados.geraCPF());
-				return c;
-			}).map(v -> clienteRepository.save(v)).forEach(System.out::println);
-		};
-	}
+	/*
+	 * @Bean CommandLineRunner init(ClienteRepository clienteRepository) { return
+	 * args -> { clienteRepository.deleteAll(); LongStream.range(1, 4).mapToObj(i ->
+	 * { Cliente c = new Cliente(); c.setNome("Cliente " + i); c.setEmail("cliente"
+	 * + i + "@email.com"); c.setGenero(GeradorDeDados.geraGenero());
+	 * c.adicionaEndereco(GeradorDeDados.geraEndereco());
+	 * c.setIdade(GeradorDeDados.geraIntegerAleatorio(18, 75));
+	 * c.setCpf(GeradorDeDados.geraCPF()); return c; }).map(v ->
+	 * clienteRepository.save(v)).forEach(System.out::println); }; }
+	 */
 }
